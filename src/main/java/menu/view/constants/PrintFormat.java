af10c2a8c47e78c14e30b9dfc971d67186912ea8
Jiihyun <cwlgusc@gmail.com>
2023-12-06 22:22:45 +0900

    fix(Menu): NONE 필드 수정

---
+++ src/main/java/menu/view/constants/PrintFormat.java
@@ -0,0 +1,10 @@
+package menu.view.constants;
+
+public enum PrintFormat {
+    FORMAT("");
+    private final String format;
+
+    PrintFormat(String format) {
+        this.format = format;
+    }
+}
