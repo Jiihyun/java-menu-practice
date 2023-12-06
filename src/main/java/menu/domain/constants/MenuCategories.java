4ed3ce3988348522fc6e0353a1cf67957a40c6e1
Jiihyun <cwlgusc@gmail.com>
2023-12-06 22:25:25 +0900

    feat(MenuCategories): 카테고리 인덱스 추가

--- src/main/java/menu/domain/constants/MenuCategories.java
+++ src/main/java/menu/domain/constants/MenuCategories.java
@@ -1,21 +1,26 @@
 package menu.domain.constants;
 
 public enum MenuCategories {
-    NONE("없음"),
-    JAPANESE("일식"),
-    KOREAN("한식"),
-    CHINESE("중식"),
-    ASIAN("아시안"),
-    WESTERN("양식"),
-    ;
+    NONE("없음", 0),
+    JAPANESE("일식", 1),
+    KOREAN("한식", 2),
+    CHINESE("중식", 3),
+    ASIAN("아시안", 4),
+    WESTERN("양식", 5);
 
     private final String categoryName;
+    private final int categoryIndex;
 
-    MenuCategories(String categoryName) {
+    MenuCategories(String categoryName, int categoryIndex) {
         this.categoryName = categoryName;
+        this.categoryIndex = categoryIndex;
     }
 
     public String getCategoryName() {
         return categoryName;
+    }
+
+    public int getCategoryIndex() {
+        return categoryIndex;
     }
 }
